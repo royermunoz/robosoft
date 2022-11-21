9a89463bb8b953ba8f1d9d7dd9f90c223ce69e8e
jquilindo@unimayor.edu.co <jquilindo@unimayor.edu.co>
2022-11-21 17:45:04 -0500

    actualizacion alan

--- lib/auth/splashScreen.dart
+++ lib/auth/splashScreen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Timer(
         const Duration(seconds: 3),
         () => Navigator.of(context).pushReplacement(MaterialPageRoute(
-            builder: (BuildContext context) => const MainPage())));
+            builder: (BuildContext context) => const LoginScreen())));
   }
 
   @override
